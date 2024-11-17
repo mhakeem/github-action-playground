79bbec8a9762fbd7464c37af3c155b14a650f6a2
Mazin Hakeem <721462+mhakeem@users.noreply.github.com>
2024-11-17 03:17:09 -0700

    post to slack channel

--- script.rb
+++ script.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'slack-ruby-client'
+
+SLACK_API_TOKEN = ENV.fetch('SLACK_API_TOKEN')
+
+Slack.configure do |config|
+  config.token = SLACK_API_TOKEN
+end
+
+client = Slack::Web::Client.new
+
+puts client.chat_postMessage(channel: '#test-channel-1', text: 'Hello World', as_user: true)
+
+
 puts "The time now is #{Time.now}"
 puts "Print secret env var #{ENV.fetch('SUPER_SECRET')}"
+
